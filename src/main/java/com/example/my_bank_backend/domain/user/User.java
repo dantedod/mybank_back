df39a5f496606d2f2bba178bb2d46387a3b2e7d8
MateusQ7 <mateusquixada@gmail.com>
2024-08-08 13:19:51 -0300

    feat: Adiciona anotação de refencia ao account

--- src/main/java/com/example/my_bank_backend/domain/user/User.java
+++ src/main/java/com/example/my_bank_backend/domain/user/User.java
@@ -1,9 +1,13 @@
 package com.example.my_bank_backend.domain.user;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -20,11 +24,15 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private Long id;
     private String name;
     private String email;
     private String phone;
     private String password;
     private String cpf;
     private String birthdate;
+    
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonBackReference
+    private Account account;
 }
