a73efa05b2857ef85da3b699fab48b37ceca7737
MateusQ7 <mateusquixada@gmail.com>
2024-08-08 13:20:36 -0300

    feat: Adiciona metodo de achar cpf no useRepository

--- src/main/java/com/example/my_bank_backend/repositories/UserRepository.java
+++ src/main/java/com/example/my_bank_backend/repositories/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.my_bank_backend.domain.user.User;
 
-public interface UserRepository extends JpaRepository<User, String>{
-
+public interface UserRepository extends JpaRepository<User, Long>{
     Optional<User> findByEmail(String email);
+    Optional<User> findByCpf(String cpf);
 }
