8320611fb54df003dab5c289f24b746a1fc76dc9
MateusQ7 <mateusquixada@gmail.com>
2024-08-08 13:17:53 -0300

    feat: Adiciona AccountController

---
+++ src/main/java/com/example/my_bank_backend/controllers/AccountController.java
@@ -0,0 +1,55 @@
+package com.example.my_bank_backend.controllers;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.my_bank_backend.domain.user.Account;
+import com.example.my_bank_backend.domain.user.User;
+import com.example.my_bank_backend.repositories.AccountRepository;
+import com.example.my_bank_backend.repositories.UserRepository;
+
+
+@RestController
+@RequestMapping("/account")
+public class AccountController {
+    
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping
+    public ResponseEntity<String> createAccount(@RequestBody Account account) {
+        Optional<User> optUser = userRepository.findByCpf(account.getCpf());
+
+        if(optUser.isPresent()) {
+            User user = optUser.get();
+
+            if(user.getAccount() != null) {
+                return ResponseEntity.badRequest().body("Já existe uma conta com esse CPF");
+            }
+
+            account.setUser(user);
+            accountRepository.save(account);
+            return ResponseEntity.ok("Conta criada com sucesso");
+        } else {
+            return ResponseEntity.badRequest().body("Fudeu, usuário não encontrado!");
+        }  
+    }
+
+    @GetMapping("/{cpf}")
+    public ResponseEntity<Account> getAccountByCpf(@PathVariable String cpf) {
+        Optional<Account> optAccount = accountRepository.findByCpf(cpf);
+
+        return optAccount.map(ResponseEntity:: ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+}
