7b239a66aa5b3b978434cc07e1de13a37c09293c
MateusQ7 <mateusquixada@gmail.com>
2024-08-14 13:11:21 -0300

    feat: Adiciona migration do card

---
+++ src/main/resources/db/migration/V5__create-table-card.sql
@@ -0,0 +1,15 @@
+-- V1__alter_users_id_to_bigint.sql
+CREATE TABLE IF NOT EXISTS cards (
+
+    id BIGSERIAL PRIMARY KEY,
+    card_name VARCHAR(255) NOT NULL UNIQUE,
+    account_id BIGSERIAL,
+    card_number VARCHAR(16) NOT NULL UNIQUE,
+    card_password VARCHAR(4) NOT NULL,
+    cvv VARCHAR(3) NOT NULL,
+    card_value VARCHAR(200) NOT NULL,
+    expiration_date VARCHAR(10) NOT NULL,
+    card_status VARCHAR(20) DEFAULT 'ACTIVE',
+    FOREIGN KEY(account_id) REFERENCES account(id)
+);
+
