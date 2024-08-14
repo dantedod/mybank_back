b53eaa5074bb2e5feb01c2fd91d1a38f75c1a5f2
MateusQ7 <mateusquixada@gmail.com>
2024-08-14 15:01:30 -0300

    fix: import

--- src/main/java/com/example/my_bank_backend/controllers/AccountController.java
+++ src/main/java/com/example/my_bank_backend/controllers/AccountController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.my_bank_backend.domain.user.Account;
+import com.example.my_bank_backend.domain.account.Account;
 import com.example.my_bank_backend.domain.user.User;
 import com.example.my_bank_backend.repositories.AccountRepository;
 import com.example.my_bank_backend.repositories.UserRepository;
