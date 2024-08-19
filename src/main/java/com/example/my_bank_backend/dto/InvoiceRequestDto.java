2a0070dfd4d0b8f4dcb1ac5d8ea424b2cd22f426
dante <danted2300@gmail.com>
2024-08-19 16:24:55 -0300

    feat:create invoice Request DTO

---
+++ src/main/java/com/example/my_bank_backend/dto/InvoiceRequestDto.java
@@ -0,0 +1,15 @@
+package com.example.my_bank_backend.dto;
+
+import java.util.Date;
+
+public record InvoiceRequestDto(
+  String invoiceDescription,
+  Double amount,
+  String cardName,
+  Date invoiceDate,
+  String invoiceStatus,
+  Long id,
+  Date dueDate,
+  String email
+  )
+{}
