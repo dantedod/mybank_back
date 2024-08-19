7a51ca56b39ed25cc32ddbc029c43d0380675fb5
dante <danted2300@gmail.com>
2024-08-19 16:21:40 -0300

    feat:add onetomany card invoice

--- src/main/java/com/example/my_bank_backend/domain/card/Card.java
+++ src/main/java/com/example/my_bank_backend/domain/card/Card.java
@@ -1,14 +1,20 @@
 package com.example.my_bank_backend.domain.card;
 
-import com.example.my_bank_backend.domain.account.Account;
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import java.util.List;
 
+import com.example.my_bank_backend.domain.account.Account;
+import com.example.my_bank_backend.domain.invoice.Invoice;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -39,4 +45,8 @@ public class Card {
     @JoinColumn(name = "account_id", referencedColumnName = "id", nullable = false)
     @JsonBackReference
     private Account account; // Cada cartão pertence a uma única conta
+    
+    @OneToMany(mappedBy = "card", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
+    private List<Invoice> invoices; // Mudado para List<Invoice>
 }
