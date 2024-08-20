1225dc6747e1557ff50c96cd75d31d5727b35a7f
MateusQ7 <mateusquixada@gmail.com>
2024-08-20 12:48:09 -0300

    fix: Refactor code of migration

--- src/main/resources/db/migration/V6__create-table-invoice.sql
+++ src/main/resources/db/migration/V6__create-table-invoice.sql
@@ -1,11 +1,12 @@
-CREATE TABLE IF NOT EXISTS invoice (
-    id BIGSERIAL PRIMARY KEY,
-    invoice_description VARCHAR(255) NOT NULL,
-    amount DOUBLE PRECISION NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    card_name VARCHAR(100) NOT NULL,
-    invoice_date DATE NOT NULL DEFAULT CURRENT_DATE,
-    invoice_status VARCHAR(20) DEFAULT 'Pendente',
-    due_date DATE NOT NULL,
-    FOREIGN KEY (card_name) REFERENCES cards(card_name)
-);
+CREATE TABLE
+    IF NOT EXISTS invoice (
+        id BIGSERIAL PRIMARY KEY,
+        invoice_description VARCHAR(255) NOT NULL,
+        amount DOUBLE PRECISION NOT NULL,
+        email VARCHAR(100) NOT NULL,
+        card_name VARCHAR(100) NOT NULL,
+        invoice_date DATE NOT NULL DEFAULT CURRENT_DATE,
+        invoice_status VARCHAR(20) DEFAULT 'Pendente',
+        due_date DATE NOT NULL,
+        FOREIGN KEY (card_name) REFERENCES cards (card_name)
+    );
