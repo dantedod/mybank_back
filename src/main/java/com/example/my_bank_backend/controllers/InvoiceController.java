0448e689e61fa913e1a1188abf8aac2757837b74
MateusQ7 <mateusquixada@gmail.com>
2024-08-28 13:57:56 -0300

    feat: Add endpoint for getInvoiceByAccount

--- src/main/java/com/example/my_bank_backend/controllers/InvoiceController.java
+++ src/main/java/com/example/my_bank_backend/controllers/InvoiceController.java
@@ -17,9 +17,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.my_bank_backend.domain.account.Account;
 import com.example.my_bank_backend.domain.card.Card;
 import com.example.my_bank_backend.domain.invoice.Invoice;
 import com.example.my_bank_backend.dto.InvoiceRequestDto;
+import com.example.my_bank_backend.repositories.AccountRepository;
 import com.example.my_bank_backend.repositories.CardRepository;
 import com.example.my_bank_backend.repositories.InvoiceRepository;
 
@@ -32,11 +34,14 @@ public class InvoiceController {
 
   private CardRepository cardRepository;
   private InvoiceRepository invoiceRepository;
+  private AccountRepository accountRepository;
 
   @Autowired
-  public InvoiceController(CardRepository cardRepository, InvoiceRepository invoiceRepository) {
+  public InvoiceController(CardRepository cardRepository, InvoiceRepository invoiceRepository,
+      AccountRepository accountRepository) {
     this.cardRepository = cardRepository;
     this.invoiceRepository = invoiceRepository;
+    this.accountRepository = accountRepository;
   }
 
   @GetMapping
@@ -45,25 +50,31 @@ public class InvoiceController {
   }
 
   @PostMapping("/{cardId}/create")
-  public ResponseEntity<String> createInvoice(@PathVariable Long cardId, @RequestBody InvoiceRequestDto invoiceDto) {
+public ResponseEntity<String> createInvoice(@PathVariable Long cardId, @RequestBody InvoiceRequestDto invoiceDto) {
     Optional<Card> optCard = cardRepository.findById(cardId);
 
     if (!optCard.isPresent()) {
-      return ResponseEntity.ok("Card not found!");
+        return ResponseEntity.badRequest().body("Card not found!");
     }
 
     Card card = optCard.get();
 
-    boolean existsInvoice = invoiceRepository.existsByCard(card);
+    // Obtenha o mês e o ano da fatura a ser criada
+    LocalDate invoiceDate = LocalDate.now(); // ou use a data fornecida por invoiceDto, se houver
+    int invoiceMonth = invoiceDate.getMonthValue();
+    int invoiceYear = invoiceDate.getYear();
+
+    // Verifique se já existe uma fatura para o cartão no mesmo mês e ano
+    boolean existsInvoice = invoiceRepository.existsByCardAndMonthAndYear(card, invoiceMonth, invoiceYear);
     if (existsInvoice) {
-      return ResponseEntity.badRequest().body("An invoice already exists for this card");
+        return ResponseEntity.badRequest().body("An invoice already exists for this card for the given month and year");
     }
 
     LocalDate minAllowedDate = LocalDate.of(2024, 1, 1);
     LocalDate currentDate = LocalDate.now();
 
     if (currentDate.isBefore(minAllowedDate)) {
-      return ResponseEntity.badRequest().body("You cannot create an invoice before the minimum allowed date!");
+        return ResponseEntity.badRequest().body("You cannot create an invoice before the minimum allowed date!");
     }
 
     Invoice newInvoice = new Invoice();
@@ -73,15 +84,17 @@ public class InvoiceController {
     newInvoice.setAmount(invoiceDto.amount());
     newInvoice.setEmail(invoiceDto.email());
 
-    Date invoiceDate = Date.from(currentDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
-    newInvoice.setInvoiceDate(invoiceDate);
+    Date invoiceStartDate = Date.from(currentDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+    newInvoice.setInvoiceDate(invoiceStartDate);
 
     Date dueDate = Date.from(currentDate.plusDays(30).atStartOfDay(ZoneId.systemDefault()).toInstant());
     newInvoice.setDueDate(dueDate);
 
     newInvoice.setInvoiceStatus(invoiceDto.invoiceStatus());
+    invoiceRepository.save(newInvoice);
     return ResponseEntity.ok("Successful invoice creation");
-  }
+}
+
 
   @PostMapping("/addvalue/{invoiceId}/{value}")
   public ResponseEntity<String> addValue(@PathVariable Long invoiceId, @PathVariable Double value) {
@@ -128,5 +141,36 @@ public class InvoiceController {
     } else {
       return ResponseEntity.notFound().build();
     }
+  }
+
+  @GetMapping("/account/{accountCpf}")
+  public ResponseEntity<Optional<Invoice>> getInvoiceByAccount(@PathVariable String accountCpf) {
+
+    Optional<Account> optAccount = accountRepository.findByCpf(accountCpf);
+
+    if (optAccount.isPresent()) {
+      Optional<Card> optCard = cardRepository.findCardByAccountCpf(accountCpf);
+
+      if (optCard.isPresent()) {
+        Long cardId = optCard.get().getId();
+
+        LocalDate currentDate = LocalDate.now();
+        int currentMonth = currentDate.getMonthValue();
+        int currentYear = currentDate.getYear();
+
+        List<Invoice> invoices = invoiceRepository.findByDateMonthAndYear(currentMonth, currentYear);
+
+        Optional<Invoice> optInvoice = invoices.stream()
+            .filter(invoice -> invoice.getCard().getId().equals(cardId))
+            .findFirst();
+
+        if (optInvoice.isPresent()) {
+          return ResponseEntity.ok(optInvoice);
+        }
+        return ResponseEntity.notFound().build();
+      }
+      return ResponseEntity.badRequest().body(null);
     }
+    return ResponseEntity.notFound().build();
+  }
 }
