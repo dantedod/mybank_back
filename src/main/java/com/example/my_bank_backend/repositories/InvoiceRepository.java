32e039fb6723282c78841c2eeb8ad0fb091b5474
dante <danted2300@gmail.com>
2024-08-28 14:12:41 -0300

    fix:teste

--- src/main/java/com/example/my_bank_backend/repositories/InvoiceRepository.java
+++ src/main/java/com/example/my_bank_backend/repositories/InvoiceRepository.java
@@ -1,6 +1,7 @@
 package com.example.my_bank_backend.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -16,8 +17,13 @@ public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
   @Query("SELECT i FROM Invoice i WHERE EXTRACT(MONTH FROM i.invoiceDate) = :month AND EXTRACT(YEAR FROM i.invoiceDate) = :year")
   List<Invoice> findByDateMonthAndYear(@Param("month") int month, @Param("year") int year);
 
+  @Query("SELECT COUNT(i) > 0 FROM Invoice i WHERE i.card = :card AND EXTRACT(MONTH FROM i.invoiceDate) = :month AND EXTRACT(YEAR FROM i.invoiceDate) = :year")
+  boolean existsByCardAndMonthAndYear(@Param("card") Card card, @Param("month") int month, @Param("year") int year);
+
   boolean existsByCard(Card card);
 
+  Optional<Invoice> findInvoiceByCardId(Long cardId);
 
-  List<Invoice> findInvoicesByCardId(Long cardId);
+    List<Invoice> findInvoicesByCardId(Long cardId);
+
 }
