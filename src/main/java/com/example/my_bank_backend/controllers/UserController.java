ab0f46e2b2dce9f344dfd82112d38bf1681413f4
dante <danted2300@gmail.com>
2024-08-30 16:57:00 -0300

    feat: Add new features for UserController

--- src/main/java/com/example/my_bank_backend/controllers/UserController.java
+++ src/main/java/com/example/my_bank_backend/controllers/UserController.java
@@ -1,18 +1,61 @@
 package com.example.my_bank_backend.controllers;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.my_bank_backend.domain.user.User;
+import com.example.my_bank_backend.dto.ConfigDetailsDto;
+import com.example.my_bank_backend.repositories.UserRepository;
+import com.example.my_bank_backend.service.UserService;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
-    @GetMapping
-    public ResponseEntity<String> getUser() {
-        return ResponseEntity.ok("Sucesso!");
+  private final UserRepository userRepository;
+  private final UserService userService;
+
+  @Autowired
+  public UserController(UserRepository userRepository, UserService userService){
+    this.userRepository = userRepository;
+    this.userService = userService;
+  }
+
+  @GetMapping
+  public ResponseEntity<String> getUser() {
+    return ResponseEntity.ok("Sucesso!");
+  }
+
+  @GetMapping("/{cpf}")
+  public ResponseEntity<User> getUserByCpf(@PathVariable String cpf) {
+    Optional<User> optUser = this.userRepository.findByCpf(cpf);
+
+    if (optUser.isPresent()) {
+      return ResponseEntity.ok(optUser.get());
+    } else {
+      return ResponseEntity.notFound().build();
     }
+
+  }
+
+    @PutMapping("/update")
+    public ResponseEntity<ConfigDetailsDto> updateUser(@RequestBody ConfigDetailsDto requestDto) {
+        boolean result = userService.updateUser(requestDto);
+        if (result) {
+            return ResponseEntity.ok(requestDto);
+        } else {
+            return ResponseEntity.notFound().build(); 
+        }
+    }
+  
 }
