419347a961aaa0d5cb314c90f10ae6cfeb3f332e
MateusQ7 <mateusquixada@gmail.com>
2024-09-18 12:47:09 -0300

    feat: Add accountService to accountController

--- src/main/java/com/example/my_bank_backend/controllers/AccountController.java
+++ src/main/java/com/example/my_bank_backend/controllers/AccountController.java
@@ -1,7 +1,6 @@
 package com.example.my_bank_backend.controllers;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,14 +8,11 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.my_bank_backend.domain.account.Account;
-import com.example.my_bank_backend.domain.user.User;
-import com.example.my_bank_backend.repositories.AccountRepository;
-import com.example.my_bank_backend.repositories.UserRepository;
+import com.example.my_bank_backend.service.AccountService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -26,80 +22,31 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AccountController {
 
-    private AccountRepository accountRepository;
-    private UserRepository userRepository;
+    private AccountService accountService;
 
     @Autowired
-    public AccountController(AccountRepository accountRepository, UserRepository userRepository){
-        this.accountRepository = accountRepository;
-        this.userRepository = userRepository;
-    }
-
-    @PostMapping
-    public ResponseEntity<String> createAccount(@RequestBody Account account) {
-        Optional<User> optUser = userRepository.findByCpf(account.getCpf());
-
-        if (optUser.isPresent()) {
-            User user = optUser.get();
-
-            if (user.getAccount() != null) {
-                return ResponseEntity.badRequest().body("Já existe uma conta com esse CPF");
-            }
-
-            account.setUser(user);
-            accountRepository.save(account);
-            return ResponseEntity.ok("Conta criada com sucesso");
-        } else {
-            return ResponseEntity.badRequest().body("Fudeu, usuário não encontrado!");
-        }
+    public AccountController(AccountService accountService){
+        this.accountService = accountService;
     }
 
     @GetMapping("/{cpf}")
     public ResponseEntity<Account> getAccountByCpf(@PathVariable String cpf) {
-        Optional<Account> optAccount = accountRepository.findByCpf(cpf);
-
-        return optAccount.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+        return accountService.getAccountByCpf(cpf);
     }
 
     @GetMapping
     public ResponseEntity<List<Account>> getAllAccounts() {
-        List<Account> accounts = accountRepository.findAll();
-        return ResponseEntity.ok(accounts);
+        return accountService.getAllAccounts();
     }
 
     @PostMapping("/{cpf}/{value}")
     public ResponseEntity<Account> addValueToAccount(@PathVariable String cpf, @PathVariable Double value) {
-
-        Optional<Account> optAccount = accountRepository.findByCpf(cpf);
-
-        if (value == null) {
-            return ResponseEntity.badRequest().body(null);
-        }
-
-        Account account = optAccount.get();
-        account.setAccountValue(account.getAccountValue() + value);
-
-        Account updatedAccount = accountRepository.save(account);
-
-        return ResponseEntity.ok(updatedAccount);
+        return accountService.addValueToAccount(cpf, value);
     }
 
     @PostMapping("/{cpf}/{value}/sub")
     public ResponseEntity<Account> subValueAccount(@PathVariable String cpf, @PathVariable Double value) {
-
-        Optional<Account> optAccount = accountRepository.findByCpf(cpf);
-
-        if (value == null) {
-            return ResponseEntity.badRequest().body(null);
-        }
-
-        Account account = optAccount.get();
-        account.setAccountValue(account.getAccountValue() - value);
-
-        Account updatedAccount = accountRepository.save(account);
-
-        return ResponseEntity.ok(updatedAccount);
-
+        return accountService.subValueAccount(cpf, value);
     }
 
 }
