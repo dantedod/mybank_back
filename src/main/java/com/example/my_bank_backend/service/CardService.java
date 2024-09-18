bcc80a25fd2f10a8a9406a545b6de2623b88dace
MateusQ7 <mateusquixada@gmail.com>
2024-09-18 13:04:36 -0300

    feat: Add CardService to project

--- src/main/java/com/example/my_bank_backend/service/CardService.java
+++ src/main/java/com/example/my_bank_backend/service/CardService.java
@@ -1,19 +1,25 @@
 package com.example.my_bank_backend.service;
 
 import java.security.SecureRandom;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.my_bank_backend.domain.account.Account;
 import com.example.my_bank_backend.domain.card.Card;
+import com.example.my_bank_backend.domain.invoice.Invoice;
 import com.example.my_bank_backend.dto.CardRequestDto;
 import com.example.my_bank_backend.repositories.AccountRepository;
 import com.example.my_bank_backend.repositories.CardRepository;
+import com.example.my_bank_backend.repositories.InvoiceRepository;
 
 @Service
 public class CardService {
@@ -24,14 +30,17 @@ public class CardService {
 
     private final AccountRepository accountRepository;
     private final CardRepository cardRepository;
+    private final InvoiceRepository invoiceRepository;
 
-    @Autowired
-    public CardService(AccountRepository accountRepository, CardRepository cardRepository) {
+    public CardService(AccountRepository accountRepository, CardRepository cardRepository,
+            InvoiceRepository invoiceRepository) {
         this.accountRepository = accountRepository;
         this.cardRepository = cardRepository;
+        this.invoiceRepository = invoiceRepository;
     }
 
-    public ResponseEntity<CardRequestDto> createCard(String accountCpf, String cardName, String cardPassword, Double cardValue) {
+    public ResponseEntity<CardRequestDto> createCard(String accountCpf, String cardName, String cardPassword,
+            Double cardValue) {
         Optional<Account> optAccount = accountRepository.findByCpf(accountCpf);
 
         if (optAccount.isPresent()) {
@@ -64,8 +73,7 @@ public class CardService {
                     cvv,
                     cardValue,
                     "10/2030",
-                    "Ativo"
-            );
+                    "Ativo");
 
             return ResponseEntity.ok(cardDto);
         } else {
@@ -95,5 +103,80 @@ public class CardService {
 
         generatedCvv.add(cvv);
         return cvv;
+    }
+
+    public ResponseEntity<List<Card>> getCardByAccountCpf(@PathVariable String accountCpf) {
+        List<Card> cards = cardRepository.findCardsByAccountCpf(accountCpf);
+        return ResponseEntity.ok(cards);
+    }
+
+    public ResponseEntity<String> buyWithCard(@PathVariable Long cardId, @PathVariable String accountCpf,
+            @PathVariable Double purchaseAmount) {
+        Optional<Card> optCard = cardRepository.findById(cardId);
+        Optional<Account> optAccount = accountRepository.findByCpf(accountCpf);
+
+        if (!optAccount.isPresent()) {
+            return ResponseEntity.badRequest().body("Account not found!");
+        }
+
+        Account account = optAccount.get();
+        LocalDate invoiceDate = LocalDate.now();
+        int invoiceMonth = invoiceDate.getMonthValue();
+        int invoiceYear = invoiceDate.getYear();
+
+        Optional<Invoice> existsInvoice = invoiceRepository.findByAccountAndMonthAndYear(account, invoiceMonth,
+                invoiceYear);
+
+        if (optCard.isPresent()) {
+            Card card = optCard.get();
+
+            if (account.getCreditLimit() - account.getUsedLimit() >= purchaseAmount) {
+                account.setUsedLimit(account.getUsedLimit() + purchaseAmount);
+                card.setCardValue(card.getCardValue() - purchaseAmount);
+
+                cardRepository.save(card);
+                accountRepository.save(account);
+
+                if (existsInvoice.isEmpty()) {
+                    LocalDate minAllowedDate = LocalDate.of(2024, 1, 1);
+                    LocalDate currentDate = LocalDate.now();
+
+                    if (currentDate.isBefore(minAllowedDate)) {
+                        return ResponseEntity.badRequest()
+                                .body("You cannot create an invoice before the minimum allowed date!");
+                    }
+
+                    Invoice newInvoice = new Invoice();
+                    newInvoice.setAccount(account);
+                    newInvoice.setAmount(purchaseAmount);
+                    newInvoice.setEmail(account.getUser().getEmail());
+                    Date invoiceStartDate = Date.from(currentDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+                    newInvoice.setInvoiceDate(invoiceStartDate);
+                    Date dueDate = Date.from(currentDate.plusDays(30).atStartOfDay(ZoneId.systemDefault()).toInstant());
+                    newInvoice.setDueDate(dueDate);
+                    newInvoice.setInvoiceStatus("Não paga!");
+                    newInvoice.setInvoiceDescription("Fatura do mês: " + invoiceMonth);
+
+                    invoiceRepository.save(newInvoice);
+                    return ResponseEntity.ok("Invoice created");
+                } else {
+                    Invoice existingInvoice = existsInvoice.get();
+                    existingInvoice.setAmount(existingInvoice.getAmount() + purchaseAmount);
+                    invoiceRepository.save(existingInvoice);
+                    return ResponseEntity.ok("Invoice updated");
+                }
+            }
+            return ResponseEntity.badRequest().body("Insufficient limit!");
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    public ResponseEntity<Void> deleteCard(@PathVariable Long cardId) {
+        if (!cardRepository.existsById(cardId)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        cardRepository.deleteById(cardId);
+        return ResponseEntity.noContent().build();
     }
 }
