fc5242dca7a42b3089ff37c0969aed1873d0ec3c
MateusQ7 <mateusquixada@gmail.com>
2024-09-25 17:24:31 -0300

    feat: Add ReqArgsController and method for createAccountForUser

--- src/main/java/com/example/my_bank_backend/service/AccountService.java
+++ src/main/java/com/example/my_bank_backend/service/AccountService.java
@@ -7,21 +7,31 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.example.my_bank_backend.domain.account.Account;
+import com.example.my_bank_backend.domain.user.User;
 import com.example.my_bank_backend.repositories.AccountRepository;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class AccountService {
 
     private final AccountRepository accountRepository;
-
-    public AccountService(AccountRepository accountRepository) {
-        this.accountRepository = accountRepository;
-    }
 
     public ResponseEntity<Account> getAccountByCpf(String cpf) {
         Optional<Account> optAccount = accountRepository.findByCpf(cpf);
 
         return optAccount.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    public Account createAccountForUser(String cpf, User newUser) {
+        Account newAccount = new Account();
+        newAccount.setCpf(cpf);
+        newAccount.setCreditLimit(1000.0);
+        newAccount.setAccountValue(0.0);
+        newAccount.setUsedLimit(0.0);
+        newAccount.setUser(newUser);
+        return newAccount;
     }
 
     public ResponseEntity<List<Account>> getAllAccounts() {
