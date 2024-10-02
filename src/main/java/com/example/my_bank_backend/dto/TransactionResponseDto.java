10725c99f68dcda0b285c2916925973f438cca4e
MateusQ7 <mateusquixada@gmail.com>
2024-10-02 18:22:39 -0300

    refactor: change variables for TransactionResponseDto

--- src/main/java/com/example/my_bank_backend/dto/TransactionResponseDto.java
+++ src/main/java/com/example/my_bank_backend/dto/TransactionResponseDto.java
@@ -2,16 +2,11 @@ package com.example.my_bank_backend.dto;
 
 import java.time.LocalDateTime;
 
-import com.example.my_bank_backend.domain.enums.TransactionEnum;
-
 public record TransactionResponseDto(
-        Long id,
-        String cpfSender,
-        String senderName,
-        String cpfReceiver,
-        String receiverName,
-        Double amount,
-        String paymentDescription,
-        LocalDateTime transactionDate,
-        TransactionEnum transactionType) {
+                Long id,
+                Long accountId,
+                Long cardId,
+                Double amount,
+                String paymentDescription,
+                LocalDateTime transactionDate) {
 }
