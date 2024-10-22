09288906bd11bb837ce19fd18ba6f4308c1ccc78
MateusQ7 <mateusquixada@gmail.com>
2024-10-22 14:37:37 -0300

    feat: add code refactor for response and tests

--- src/main/java/com/example/my_bank_backend/dto/PaymentInvoiceDto.java
+++ src/main/java/com/example/my_bank_backend/dto/PaymentInvoiceDto.java
@@ -3,5 +3,4 @@ package com.example.my_bank_backend.dto;
 public record PaymentInvoiceDto(
   String accountCpf,
   Double payValue ){
-
-  }
+}
--- src/main/java/com/example/my_bank_backend/service/AuthenticationService.java
+++ src/main/java/com/example/my_bank_backend/service/AuthenticationService.java
@@ -1,8 +1,8 @@
 package com.example.my_bank_backend.service;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.example.my_bank_backend.domain.user.User;
 import com.example.my_bank_backend.dto.LoginRequestDto;
 import com.example.my_bank_backend.dto.RegisterRequestDto;
 import com.example.my_bank_backend.dto.ResponseDto;
@@ -16,11 +16,11 @@ public class AuthenticationService {
     private final AuthService authService;
     private final UserService userService;
 
-    public ResponseEntity<ResponseDto> login(LoginRequestDto body) {
+    public ResponseDto login(LoginRequestDto body) {
         return authService.login(body.email(), body.password());
     }
 
-    public ResponseEntity<ResponseDto> register(RegisterRequestDto body) {
+    public User register(RegisterRequestDto body) {
         return userService.register(body.name(), body.email(), body.password(), body.phone(), body.cpf(), body.birthdate());
     }
 }
