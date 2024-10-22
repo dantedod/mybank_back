5fdc6fbf36971badd73bd3b6b6b78685eb12d0cd
MateusQ7 <mateusquixada@gmail.com>
2024-10-22 14:35:45 -0300

    feat: add code refactor for response and tests

--- src/main/java/com/example/my_bank_backend/controllers/AuthController.java
+++ src/main/java/com/example/my_bank_backend/controllers/AuthController.java
@@ -1,5 +1,6 @@
 package com.example.my_bank_backend.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,9 +8,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.my_bank_backend.domain.user.User;
 import com.example.my_bank_backend.dto.LoginRequestDto;
 import com.example.my_bank_backend.dto.RegisterRequestDto;
 import com.example.my_bank_backend.dto.ResponseDto;
+import com.example.my_bank_backend.exception.IncorrectPasswordException;
+import com.example.my_bank_backend.exception.UserNotFoundException;
 import com.example.my_bank_backend.service.AuthenticationService;
 
 import lombok.RequiredArgsConstructor;
@@ -24,12 +28,37 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<ResponseDto> login(@RequestBody LoginRequestDto body) {
-        return authenticationService.login(body);
+
+        try {
+            ResponseDto responseDto = authenticationService.login(body);
+
+            if (responseDto != null) {
+                return ResponseEntity.ok(responseDto);
+            }
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+
+        } catch (UserNotFoundException | IncorrectPasswordException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
 
     @PostMapping("/register")
     public ResponseEntity<ResponseDto> register(@RequestBody RegisterRequestDto body) {
-        return authenticationService.register(body);
-    }
 
+        try {
+            User user = authenticationService.register(body);
+
+            if (user != null) {
+                return ResponseEntity.ok(new ResponseDto(null, user.getName(), user.getCpf()));
+            }
+            
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        } catch (Exception e) {
+            return ResponseEntity
+                    .badRequest()
+                    .build();
+        }
+    }
 }
--- src/main/java/com/example/my_bank_backend/service/AuthService.java
+++ src/main/java/com/example/my_bank_backend/service/AuthService.java
@@ -1,12 +1,12 @@
 package com.example.my_bank_backend.service;
 
-
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.my_bank_backend.domain.user.User;
 import com.example.my_bank_backend.dto.ResponseDto;
+import com.example.my_bank_backend.exception.IncorrectPasswordException;
+import com.example.my_bank_backend.exception.UserNotFoundException;
 import com.example.my_bank_backend.infra.security.TokenService;
 import com.example.my_bank_backend.repositories.UserRepository;
 
@@ -20,14 +20,17 @@ public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final TokenService tokenService;
 
-    public ResponseEntity<ResponseDto> login(String email, String password) {
+    public ResponseDto login(String email, String password) {
+
         User user = this.userRepository.findByEmail(email)
-                .orElseThrow(() -> new RuntimeException("Usuário não encontrado!"));
-        if (passwordEncoder.matches(password, user.getPassword())) {
-            String token = this.tokenService.generateToken(user);
-            return ResponseEntity.ok(new ResponseDto(token, user.getName(), user.getCpf()));
+                .orElseThrow(() -> new UserNotFoundException("User not Found!"));
+
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            throw new IncorrectPasswordException("Incorrect Password!");
         }
-        return ResponseEntity.badRequest().build();
+
+        String token = this.tokenService.generateToken(user);
+        return new ResponseDto(token, user.getName(), user.getCpf());
     }
 
 }
