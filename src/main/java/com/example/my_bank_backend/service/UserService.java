3062af04325bd30788130812405eb814e2b190db
MateusQ7 <mateusquixada@gmail.com>
2024-10-24 15:01:33 -0300

    feat: add emailexception for email and cpf already in use

--- src/main/java/com/example/my_bank_backend/service/UserService.java
+++ src/main/java/com/example/my_bank_backend/service/UserService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 
 import com.example.my_bank_backend.domain.user.User;
 import com.example.my_bank_backend.dto.ConfigDetailsDto;
+import com.example.my_bank_backend.exception.CpfAlreadyExistException;
+import com.example.my_bank_backend.exception.EmailAlreadyExistException;
 import com.example.my_bank_backend.repositories.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -18,7 +20,6 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final AccountService accountService;
-
 
     public ConfigDetailsDto updateUser(ConfigDetailsDto requestDto) {
         User user = userRepository.findByCpf(requestDto.cpf()).orElse(null);
@@ -65,23 +66,28 @@ public class UserService {
     public User register(String name, String email, String password, String phone, String cpf,
             String birthDate) {
 
-        Optional<User> user = userRepository.findByEmail(email);
+        Optional<User> userEmail = userRepository.findByEmail(email);
+        Optional<User> userCpf = userRepository.findByCpf(cpf);
 
-        if (user.isEmpty()) {
-            User newUser = new User();
-            newUser.setPassword(passwordEncoder.encode(password));
-            newUser.setEmail(email);
-            newUser.setName(name);
-            newUser.setPhone(phone);
-            newUser.setCpf(cpf);
-            newUser.setBirthdate(birthDate);
-
-            newUser.setAccount(accountService.createAccountForUser(cpf, newUser));
-
-            this.userRepository.save(newUser);
-            return newUser;
+        if (userEmail.isPresent()) {
+            throw new EmailAlreadyExistException("Email já existe!");
         }
-        return null;
-    }
 
+        if (userCpf.isPresent()) {
+            throw new CpfAlreadyExistException("Cpf já existe!");
+        }
+
+        User newUser = new User();
+        newUser.setPassword(passwordEncoder.encode(password));
+        newUser.setEmail(email);
+        newUser.setName(name);
+        newUser.setPhone(phone);
+        newUser.setCpf(cpf);
+        newUser.setBirthdate(birthDate);
+
+        newUser.setAccount(accountService.createAccountForUser(cpf, newUser));
+
+        this.userRepository.save(newUser);
+        return newUser;
+    }
 }
