9ce4114790c9183d5d61bfbcb7acb49f6a50176f
Dante <danted2300@gmail.com>
2024-10-28 12:03:35 -0300

    feat/ add validation if payvalue > invoicevalue

--- src/main/java/com/example/my_bank_backend/service/InvoiceService.java
+++ src/main/java/com/example/my_bank_backend/service/InvoiceService.java
@@ -19,132 +19,137 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class InvoiceService {
 
-    private final InvoiceRepository invoiceRepository;
-    private final AccountRepository accountRepository;
+  private final InvoiceRepository invoiceRepository;
+  private final AccountRepository accountRepository;
 
-    public List<Invoice> getAllInvoices(String accountCpf) {
-        return invoiceRepository.findByAccountCpf(accountCpf);
+  public List<Invoice> getAllInvoices(String accountCpf) {
+    return invoiceRepository.findByAccountCpf(accountCpf);
+  }
+
+  public String createInvoice(Account account, Double purchaseAmount, int invoiceMonth, int invoiceYear) {
+    if (account == null) {
+      return "Account is null";
     }
 
-    public String createInvoice(Account account, Double purchaseAmount, int invoiceMonth, int invoiceYear) {
-        if (account == null) {
-            return "Account is null";
-        }
-        
-        Optional<Invoice> existsInvoice = invoiceRepository.findByAccountAndMonthAndYear(account, invoiceMonth, invoiceYear);
-        
-        if (existsInvoice.isEmpty()) {
-            Invoice newInvoice = new Invoice();
-            newInvoice.setAccount(account);
-            newInvoice.setAmount(purchaseAmount);
-            newInvoice.setEmail(account.getUser().getEmail());
-            newInvoice.setInvoiceDate(Date.from(LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toInstant()));
-            newInvoice.setDueDate(Date.from(LocalDate.now().plusDays(30).atStartOfDay(ZoneId.systemDefault()).toInstant()));
-            newInvoice.setClosingDate(Date.from(LocalDate.now().plusDays(24).atStartOfDay(ZoneId.systemDefault()).toInstant()));
-            newInvoice.setInvoiceStatus("Unpaid!");
-            newInvoice.setInvoiceDescription("Month: " + invoiceMonth);
-            
-            try {
-                invoiceRepository.save(newInvoice);
-                return "SUCCESS";
-            } catch (Exception e) {
-                return "Failed to create invoice: " + e.getMessage();
-            }
-        } else {
-            Invoice existingInvoice = existsInvoice.get();
-            existingInvoice.setAmount(existingInvoice.getAmount() + purchaseAmount);
-            invoiceRepository.save(existingInvoice);
-            return "Invoice updated";
-        }
+    Optional<Invoice> existsInvoice = invoiceRepository.findByAccountAndMonthAndYear(account, invoiceMonth,
+        invoiceYear);
+
+    if (existsInvoice.isEmpty()) {
+      Invoice newInvoice = new Invoice();
+      newInvoice.setAccount(account);
+      newInvoice.setAmount(purchaseAmount);
+      newInvoice.setEmail(account.getUser().getEmail());
+      newInvoice.setInvoiceDate(Date.from(LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toInstant()));
+      newInvoice.setDueDate(Date.from(LocalDate.now().plusDays(30).atStartOfDay(ZoneId.systemDefault()).toInstant()));
+      newInvoice
+          .setClosingDate(Date.from(LocalDate.now().plusDays(24).atStartOfDay(ZoneId.systemDefault()).toInstant()));
+      newInvoice.setInvoiceStatus("Unpaid!");
+      newInvoice.setInvoiceDescription("Month: " + invoiceMonth);
+
+      try {
+        invoiceRepository.save(newInvoice);
+        return "SUCCESS";
+      } catch (Exception e) {
+        return "Failed to create invoice: " + e.getMessage();
+      }
+    } else {
+      Invoice existingInvoice = existsInvoice.get();
+      existingInvoice.setAmount(existingInvoice.getAmount() + purchaseAmount);
+      invoiceRepository.save(existingInvoice);
+      return "Invoice updated";
     }
-    
+  }
 
-    public String addValue(Long invoiceId, Double value) {
-        Optional<Invoice> optInvoice = invoiceRepository.findById(invoiceId);
+  public String addValue(Long invoiceId, Double value) {
+    Optional<Invoice> optInvoice = invoiceRepository.findById(invoiceId);
 
-        if (optInvoice.isEmpty()) {
-            return "Invoice not Found!";
-        }
-
-        Invoice existingInvoice = optInvoice.get();
-        Date invoiceDate = existingInvoice.getInvoiceDate();
-
-        LocalDate invoiceLocalDate = new java.sql.Date(invoiceDate.getTime()).toLocalDate();
-        int invoiceMonth = invoiceLocalDate.getMonthValue();
-        int invoiceYear = invoiceLocalDate.getYear();
-
-        List<Invoice> invoices = invoiceRepository.findByDateMonthAndYear(invoiceMonth, invoiceYear);
-
-        Optional<Invoice> matchingInvoice = invoices.stream()
-                .filter(invoice -> invoice.getId().equals(invoiceId))
-                .findFirst();
-
-        if (matchingInvoice.isEmpty()) {
-            return "Invoice don't match with month and year.";
-        }
-
-        Double existingAmount = existingInvoice.getAmount();
-        Double newAmount = existingAmount + value;
-
-        existingInvoice.setAmount(newAmount);
-        invoiceRepository.save(existingInvoice);
-
-        return "Value added to invoice";
+    if (optInvoice.isEmpty()) {
+      return "Invoice not Found!";
     }
 
-    public Invoice getInvoiceByAccount(String accountCpf) {
-        Optional<Account> optAccount = accountRepository.findByCpf(accountCpf);
+    Invoice existingInvoice = optInvoice.get();
+    Date invoiceDate = existingInvoice.getInvoiceDate();
 
-        if (optAccount.isPresent()) {
-            long accountId = optAccount.get().getId();
-            LocalDate currentDate = LocalDate.now();
-            int currentMonth = currentDate.getMonthValue();
-            int currentYear = currentDate.getYear();
+    LocalDate invoiceLocalDate = new java.sql.Date(invoiceDate.getTime()).toLocalDate();
+    int invoiceMonth = invoiceLocalDate.getMonthValue();
+    int invoiceYear = invoiceLocalDate.getYear();
 
-            List<Invoice> invoices = invoiceRepository.findByDateMonthAndYear(currentMonth, currentYear);
+    List<Invoice> invoices = invoiceRepository.findByDateMonthAndYear(invoiceMonth, invoiceYear);
 
-            Optional<Invoice> optInvoice = invoices.stream()
-                    .filter(invoice -> invoice.getAccount().getId().equals(accountId))
-                    .findFirst();
+    Optional<Invoice> matchingInvoice = invoices.stream()
+        .filter(invoice -> invoice.getId().equals(invoiceId))
+        .findFirst();
 
-            if (optInvoice.isPresent()) {
-                return optInvoice.get();
-            }
-        }
-        return null;
+    if (matchingInvoice.isEmpty()) {
+      return "Invoice don't match with month and year.";
     }
 
-    public Invoice payInvoice(String accountCpf, Double payValue) {
+    Double existingAmount = existingInvoice.getAmount();
+    Double newAmount = existingAmount + value;
 
-        Optional<Account> optAccount = accountRepository.findByCpf(accountCpf);
-        if (optAccount.isEmpty()) {
-            return null;
-        }
-    
-        Account account = optAccount.get();
-    
-        Optional<Invoice> optInvoice = invoiceRepository.findInvoiceByAccountId(account.getId());
-        if (optInvoice.isEmpty()) {
-            return null;
-        }
+    existingInvoice.setAmount(newAmount);
+    invoiceRepository.save(existingInvoice);
 
-        Invoice payInvoice = optInvoice.get();
-    
-        LocalDate actualDay = LocalDate.now();
-        Date payDay = Date.from(actualDay.atStartOfDay(ZoneId.systemDefault()).toInstant());
-    
-        if (payDay.compareTo(payInvoice.getDueDate()) <= 0) {
+    return "Value added to invoice";
+  }
 
-            account.setAccountValue(account.getAccountValue() - payValue);
-            payInvoice.setAmount(payInvoice.getAmount() - payValue);
-            
-            account.setUsedLimit(account.getUsedLimit() - payValue);
-        }
-    
-        invoiceRepository.save(payInvoice);
-        accountRepository.save(account);
-    
-        return payInvoice;
+  public Invoice getInvoiceByAccount(String accountCpf) {
+    Optional<Account> optAccount = accountRepository.findByCpf(accountCpf);
+
+    if (optAccount.isPresent()) {
+      long accountId = optAccount.get().getId();
+      LocalDate currentDate = LocalDate.now();
+      int currentMonth = currentDate.getMonthValue();
+      int currentYear = currentDate.getYear();
+
+      List<Invoice> invoices = invoiceRepository.findByDateMonthAndYear(currentMonth, currentYear);
+
+      Optional<Invoice> optInvoice = invoices.stream()
+          .filter(invoice -> invoice.getAccount().getId().equals(accountId))
+          .findFirst();
+
+      if (optInvoice.isPresent()) {
+        return optInvoice.get();
+      }
+    }
+    return null;
+  }
+
+  public Invoice payInvoice(String accountCpf, Double payValue) {
+
+    Optional<Account> optAccount = accountRepository.findByCpf(accountCpf);
+    if (optAccount.isEmpty()) {
+      return null;
+    }
+
+    Account account = optAccount.get();
+
+    Optional<Invoice> optInvoice = invoiceRepository.findInvoiceByAccountId(account.getId());
+    if (optInvoice.isEmpty()) {
+      return null;
+    }
+
+    Invoice payInvoice = optInvoice.get();
+
+    if (payValue > payInvoice.getAmount()) {
+      return null;
     }
     
+    LocalDate actualDay = LocalDate.now();
+    Date payDay = Date.from(actualDay.atStartOfDay(ZoneId.systemDefault()).toInstant());
+
+    if (payDay.compareTo(payInvoice.getDueDate()) <= 0) {
+
+      account.setAccountValue(account.getAccountValue() - payValue);
+      payInvoice.setAmount(payInvoice.getAmount() - payValue);
+
+      account.setUsedLimit(account.getUsedLimit() - payValue);
+    }
+
+    invoiceRepository.save(payInvoice);
+    accountRepository.save(account);
+
+    return payInvoice;
+  }
+
 }
