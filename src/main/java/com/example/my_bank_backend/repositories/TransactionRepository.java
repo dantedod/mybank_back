d65a32401bb7cdf3fdb3aa76a8eaa7b4cf9373bf
MateusQ7 <mateusquixada@gmail.com>
2024-10-30 19:53:00 -0300

    feat: add method for findByCard

--- src/main/java/com/example/my_bank_backend/repositories/TransactionRepository.java
+++ src/main/java/com/example/my_bank_backend/repositories/TransactionRepository.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.my_bank_backend.domain.card.Card;
 import com.example.my_bank_backend.domain.transaction.Transaction;
 
 public interface TransactionRepository  extends JpaRepository<Transaction, Long>{
     List<Transaction> findByAccountCpf(String cpf);
+
+    List<Transaction> findByCard(Card card);
 }
