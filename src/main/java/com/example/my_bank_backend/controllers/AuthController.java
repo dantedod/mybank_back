86648ddfd8d9d2d66eb2e3fc98bf73366400d076
Dante <danted2300@gmail.com>
2024-11-06 15:51:58 -0300

    feat: Add new return for login

--- src/main/java/com/example/my_bank_backend/controllers/AuthController.java
+++ src/main/java/com/example/my_bank_backend/controllers/AuthController.java
@@ -1,5 +1,8 @@
 package com.example.my_bank_backend.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -26,45 +29,52 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final AuthenticationService authenticationService;
+  private final AuthenticationService authenticationService;
 
-    @PostMapping("/login")
-    public ResponseEntity<ResponseDto> login(@RequestBody LoginRequestDto body) {
+  @PostMapping("/login")
+  public ResponseEntity<Map<String, String>> login(@RequestBody LoginRequestDto body) {
 
-        try {
-            ResponseDto responseDto = authenticationService.login(body);
+    try {
+      ResponseDto responseDto = authenticationService.login(body);
+      if (responseDto != null) {
+        Map<String, String> response = new HashMap<>();
+        response.put("token", responseDto.token());
+        response.put("name", responseDto.name());
+        response.put("cpf", responseDto.cpf());
 
-            if (responseDto != null) {
-                return ResponseEntity.ok(responseDto);
-            }
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        return ResponseEntity.ok(response);
 
-        } catch (UserNotFoundException | IncorrectPasswordException e) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-        }
+      }
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+
+    } catch (UserNotFoundException e) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("message", e.getMessage()));
+    } catch (IncorrectPasswordException ipe) {
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("message", ipe.getMessage()));
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
     }
+  }
 
-    @PostMapping("/register")
-    public ResponseEntity<ResponseDto> register(@RequestBody RegisterRequestDto body) {
+  @PostMapping("/register")
+  public ResponseEntity<ResponseDto> register(@RequestBody RegisterRequestDto body) {
 
-        try {
-            User user = authenticationService.register(body);
+    try {
+      User user = authenticationService.register(body);
 
-            if (user != null) {
-                return ResponseEntity.ok(new ResponseDto(null, user.getName(), user.getCpf()));
-            }
+      if (user != null) {
+        return ResponseEntity.ok(new ResponseDto(null, user.getName(), user.getCpf()));
+      }
 
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        } catch (EmailAlreadyExistException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseDto(e.getMessage(), null, null));
-        } catch (CpfAlreadyExistException ex) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseDto(ex.getMessage(), null, null));
-        } catch (Exception e) {
-            return ResponseEntity
-                    .badRequest()
-                    .build();
-        }
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    } catch (EmailAlreadyExistException e) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseDto(e.getMessage(), null, null));
+    } catch (CpfAlreadyExistException ex) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseDto(ex.getMessage(), null, null));
+    } catch (Exception e) {
+      return ResponseEntity
+          .badRequest()
+          .build();
     }
+  }
 }
